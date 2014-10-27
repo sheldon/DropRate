e3f3dec9e3ffe539b3299788fad07bbe26a5a04d
Sheldon Els <sheldon.els@gmail.com>
2014-10-27 08:37:30 +0100

    added db

--- DropRate.js
+++ DropRate.js
@@ -1,27 +1,29 @@
 var express = require('express');
-	app = express(),
-	fs = require('fs'),
+    app = express(),
+    fs = require('fs'),
     https = require('https'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     session = require('express-session'),
-	passport = require('passport'),
-	BnetStrategy = require('passport-bnet').Strategy;
+    passport = require('passport'),
+    BnetStrategy = require('passport-bnet').Strategy,
+    sqlite3 = require('sqlite3').verbose();
+    db = new sqlite3.Database('./db/DropRate.sqlite');
 
 passport.use(new BnetStrategy({
-	clientID: "f3fmq7n39vkn97znkyrr5dz4c7f9qzxf",
-	clientSecret: "",
-	callbackURL: "https://localhost:3000/auth/bnet/callback"
+    clientID: "f3fmq7n39vkn97znkyrr5dz4c7f9qzxf",
+    clientSecret: "TQrRjmxwuNxHnTRBYWWGakNuUctn7SEB",
+    callbackURL: "https://localhost:3000/auth/bnet/callback"
 }, function (accessToken, refreshToken, profile, done){
-	return done(null, profile);
+    return done(null, profile);
 }));
 
 passport.serializeUser(function(user, done){
-	done(null, user);
+    done(null, user);
 });
 
 passport.deserializeUser(function(user, done){
-	done(null, user);
+    done(null, user);
 });
 
 app.use(express.static('public'));
@@ -39,26 +41,40 @@ app.get('/auth/bnet/callback',
         res.redirect('/');
     });
 
+db.serialize(function() {
+    db.run("CREATE TABLE IF NOT EXISTS feed (character, name, type, timestamp, itemId)");
+});
+
 app.get('/', function (req, res){
-	setInterval(function(){
-		https.request({
-			host: 'eu.api.battle.net',
-			path: '/wow/character/Chamber%20of%20Aspects/Litae?fields=feed&locale=en_GB&apikey=f3fmq7n39vkn97znkyrr5dz4c7f9qzxf'
-		}, function(response) {
-			var body = '';
+    setInterval(function(){
+        https.request({
+            host: 'eu.api.battle.net',
+            path: '/wow/character/Chamber%20of%20Aspects/Litae?fields=feed&locale=en_GB&apikey=f3fmq7n39vkn97znkyrr5dz4c7f9qzxf'
+        }, function(response) {
+            var body = '';
 
-			response.on('data', function (chunk) {
-				body += chunk;
-			});
+            response.on('data', function (chunk) {
+                body += chunk;
+            });
 
-			response.on('end', function () {
-				var feed = JSON.parse(body);
-				console.log(feed.feed.length);
-			});
-		}).end();
-	}, 2000);
+            response.on('end', function () {
+                var feed = JSON.parse(body);
+                feed.feed.forEach(function(activity){
+                    db.serialize(function() {
+                        db.run("INSERT INTO feed (character, name, type, timestamp, itemId) VALUES ($character, $name, $type, $timestamp, $itemId)", {
+                            $character: 'Litae',
+                            $name: activity.name,
+                            $type: activity.type,
+                            $timestamp: activity.timestamp,
+                            $itemId: activity.itemId
+                        });
+                    });
+                });
+            });
+        }).end();
+    }, 2000);
 
-	res.end();
+    res.end();
 });
 
 var server = https.createServer({
@@ -66,3 +82,13 @@ var server = https.createServer({
     cert: fs.readFileSync('./ssl/server.crt'),
 }, app).listen(3000);
 
+var gracefulShutdown = function() {
+    server.close(function() {
+        db.close(function(){
+            process.exit()
+        });
+    });
+}
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
